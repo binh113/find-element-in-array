d9d6ed590a3599d177f1bb8fde2230cecd238fc4
binh113 <quocbinh230597@gmail.com>
2022-10-10 11:31:35 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name's student: ");
+        String input_name = scanner.nextLine();
+        String[] students = {"Christian", "Micheal", "Camila", "Tanya", "Conor", "Zachariah", "Mallory", "Zoe", "emily"};
+
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Position of the students in the list " + input_name + " is: " + i);
+                isExist =true;
+                break;
+            }
+        }
+    if (!isExist){
+        System.out.println("not found " + input_name + " in the list.");
+
+    }
+    }
+}
